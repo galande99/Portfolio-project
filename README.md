5ecc22573b05670d95ecdf222b5f94fa3721a6c7
Abhilesh Galande <abhileshg1999@gmail.com>
2024-05-29 16:40:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Personal-Portfolio
+
+## Link - https://sonuk-mehta.vercel.app/
+Crafted a dynamic personal portfolio website showcasing skills, projects, and achievements. Responsive layout, and user-friendly interface highlight my expertise.
+
+## Key Features
+
+## Responsive Design:
+Built a responsive personal portfolio website using HTML, CSS, and JavaScript, ensuring optimal viewing experience across devices.
+
+## Portfolio Showcase: 
+Developed a visually appealing portfolio showcasing projects with descriptions, images, and live demos or GitHub repositories.
+
+## Skills Highlight: 
+Featured technical skills and expertise in a dedicated section, using graphical elements to visually represent proficiency levels.
+
+## Contact Integration: 
+Integrated contact information and social media links for easy communication and networking opportunities.
---
+++ javascript/script.js
@@ -0,0 +1,164 @@
+console.log("hello world");
+const myName = "Sonu Mehta";
+console.log(myName);
+
+////////////////////////////////////////////////////////////////
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+// Read More Button
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// form ////////////////////
+
+function send() {
+  console.log("function send call");
+}
+
+///////////////////////////////////////////////
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else if (href.startsWith("#")) {
+      // Scroll to other links
+      const sectionEl = document.querySelector(href);
+      if (sectionEl) {
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+///////////////////////////////////////
+
+console.log("JavaScript file loaded."); // Add this line to check if the file is loaded
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    console.log("Link clicked."); // Add this line to check if the link is clicked
+    // Rest of your code...
+  });
+});
